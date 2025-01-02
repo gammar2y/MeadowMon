6739c3edc3f2f872406672d8f641de3fce1f1844
gammar2y <156562743+gammar2y@users.noreply.github.com>
2025-01-01 21:43:08 -0700

    Create admin.py

---
+++ server/djangoapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Ca, CarModel
+
+# Registering models with their respective admins
+admin.site.register(CarMake)
+admin.site.register(CarModel)
+# CarModelInline class
+
+# CarModelAdmin class
+
+# CarMakeAdmin class with CarModelInline
+
+# Register models here
