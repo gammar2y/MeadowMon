9a6535d0888f8e794b122c9bbbfb085b6d3f45fc
gammar2y <156562743+gammar2y@users.noreply.github.com>
2025-01-02 14:16:27 -0700

    Create product.js

---
+++ server/database/product.js
@@ -0,0 +1,33 @@
+const { Int32 } = require('mongodb');
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+
+const cars = new Schema({
+  product_id: {
+    type: Number,
+    required: true,
+  },
+  set: {
+    type: String,
+    required: true,
+  },
+  card_type: {
+    type: String,
+    required: true,
+  },
+  bodyType: {
+    type: String,
+    required: true,
+  },
+  year: {
+    type: Number,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('product', product);
