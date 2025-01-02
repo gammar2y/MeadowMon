0111fb712eeaabb031234587416e5851133ad846
gammar2y <156562743+gammar2y@users.noreply.github.com>
2025-01-01 21:48:48 -0700

    Update app.js

--- server/djangoapp/app.js
+++ server/djangoapp/app.js
@@ -1,13 +1,12 @@
-import LoginPanel from "./components/Login/Login"
-import Register from "./components/Register/Register"
+import LoginPanel from "./components/login"
+import Register from "./components/signup"
 import { Routes, Route } from "react-router-dom";
-import get_product from './components/Product/get_product';
-import cart from "./components/orders/cart";
-import checkout from "./components/orders/checkout";
-import order_confirmation from "./components/orders/order_confirmation";
-import cart from "./components/orders/cart";
-import requests from “./components/requests/requests”;
-import get_product_details from “./components/Product/get_product_details”;
+import get_product from './store/product';
+import cart from "./store/cart";
+import checkout from "./store/checkout";
+import order_confirmation from "./store/orders";
+import requests from “./store/requests”;
+import get_product_details from “./store/product”;
 function App() {
   return (
     <Routes>
