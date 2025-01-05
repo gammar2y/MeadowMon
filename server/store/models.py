33447eae2e2d528751e58d063558c82f1d343e5c
gammar2y <156562743+gammar2y@users.noreply.github.com>
2025-01-05 16:38:00 -0700

    Create models.py

---
+++ server/store/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Product(models.Model):
+    
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class CardModel(models.Model):
+    card = models.ForeignKey(product, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    CARD_TYPES = [
+        ('Graded', 'Graded'),
+        ('Singles', 'Singles'),
+        (‘Boxes’,’Boxes’)
+    ]
+    type = models.CharField(max_length=10, choices=CARD_TYPES, default='PACK’)
+    def __str__(self):
+        return self.name
