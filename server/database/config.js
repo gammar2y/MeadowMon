d16a0c02b0c43d32ebc7d61c8fcc80b438b51b31
Andrew Bell <acrew1905@gmail.com>
2025-01-19 21:37:22 -0700

    Add detailed description of the changes

--- server/database/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-    backendUrl: 'http://localhost:8000',
-    mongodb_uri: 'mongodb+srv://meadowmonbusiness:NXVwZOXBQChb01uj@cluster0.mongodb.net/MeadowMon?retryWrites=true&w=majority',
-  };
-  
-module.exports = config;
