30529629b253ae61e056ca5ff4a1c9dc1328b6e4
Andrew Bell <acrew1905@gmail.com>
2025-01-21 21:24:46 -0700

    Add detailed description of the changes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -18,12 +18,10 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'https://acrew1905-8000.theiadockernext-1-labs-prod-theiak8s-4-'
     'tor01.proxy.cognitiveclass.ai',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://acrew1905-8000.theiadockernext-1-labs-prod-theiak8s-4-'
     'tor01.proxy.cognitiveclass.ai',
 ]
 
