0252b165e6b9de7a6895a61de324b6e86caea8ed
Andrew Bell <acrew1905@gmail.com>
2025-01-24 20:10:38 -0700

    Add detailed description of the changes

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -84,16 +84,26 @@ WSGI_APPLICATION = 'djangoproj.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'yourdbname'),
-        'USER': os.getenv('POSTGRES_USER', 'yourdbuser'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'yourdbpassword'),
-        'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
-        'PORT': os.getenv('POSTGRES_PORT', '5432'),
+DATABASE_ENGINE = os.getenv('DATABASE_ENGINE', 'sqlite')
+
+if DATABASE_ENGINE == 'postgresql':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('POSTGRES_DB', 'yourdbname'),
+            'USER': os.getenv('POSTGRES_USER', 'yourdbuser'),
+            'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'yourdbpassword'),
+            'HOST': os.getenv('POSTGRES_HOST', 'localhost'),
+            'PORT': os.getenv('POSTGRES_PORT', '5432'),
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.'
